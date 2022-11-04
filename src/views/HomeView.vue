ea57c608f2094ca3dd46f6a0400285a0c142f6fa
rukeeo1 <rukeeojigbo@gmail.com>
2022-11-04 19:29:00 +0100

    chore: initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <AppHeader />
+    <div class="home__body">
+      <AppInput placeholder="Search" label="Search" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AppHeader from '@/components/AppHeader.vue';
+import AppInput from '@/components/AppInput.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    AppHeader,
+    AppInput,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.home {
+  &__body {
+
+  }
+}
+</style>
